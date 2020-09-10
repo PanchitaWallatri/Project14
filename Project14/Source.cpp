0f96fda8253acd0dc1ecbe423b0b34a3c70cfb99
PANCHITA WALLATRI <63010666@KMITL.AC.TH>
2020-09-10 20:01:29 +0700

    Add project files.

---
+++ Project14/Source.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int num, i, j, star, space;
+	printf("enter number : ");
+	scanf_s("%d", &num);
+	printf("\n");
+	for (i = 1; i <= 2 * num - 1; i++)
+	{
+		space=abs (num - i);
+		star = 2 * (num - abs(num - i)) - 1;
+		for (j = 1; j <= space; j++) printf(" ");
+		for (j = 1; j <= star; j++) printf("*");
+		printf("\n");
+	}
+	printf("\n");
+	return 0;
+}
